9afabb8e4ba3c5448e8409b2233109fd83b47741
rbtstha <robitstha90@gmail.com>
2016-09-09 17:31:51 +0545

    updated
    all names sentences to nepali

--- ci/language/nepali/ftp_lang.php
+++ ci/language/nepali/ftp_lang.php
@@ -1,17 +1,17 @@
 <?php
 
-$lang['ftp_no_connection']			= "Unable to locate a valid connection ID. Please make sure you are connected before peforming any file routines.";
-$lang['ftp_unable_to_connect']		= "Unable to connect to your FTP server using the supplied hostname.";
-$lang['ftp_unable_to_login']		= "Unable to login to your FTP server. Please check your username and password.";
-$lang['ftp_unable_to_makdir']		= "Unable to create the directory you have specified.";
-$lang['ftp_unable_to_changedir']	= "Unable to change directories.";
-$lang['ftp_unable_to_chmod']		= "Unable to set file permissions. Please check your path.";
-$lang['ftp_unable_to_upload']		= "Unable to upload the specified file. Please check your path.";
-$lang['ftp_unable_to_download']		= "Unable to download the specified file. Please check your path.";
-$lang['ftp_no_source_file']			= "Unable to locate the source file. Please check your path.";
-$lang['ftp_unable_to_rename']		= "Unable to rename the file.";
-$lang['ftp_unable_to_delete']		= "Unable to delete the file.";
-$lang['ftp_unable_to_move']			= "Unable to move the file. Please make sure the destination directory exists.";
+$lang['ftp_no_connection']			= "मान्य सम्पर्क आइडी भेटाउन सकिएन ।  फाइलको कार्य गर्न सम्पर्कमा जोडिनु होला।";
+$lang['ftp_unable_to_connect']		= "दिनुभएको होस्ट नाम बाट एफ टि पि सर्भर सँग जडान हुन सकेन ।    ";
+$lang['ftp_unable_to_login']		= "तपाईंको एफ टि पि सर्भरमा लग इन हुन सकेन। कृपया युजरनेम र पासवोर्ड जाच गर्नु होला। ";
+$lang['ftp_unable_to_makdir']		= "तपाईंले तोक्नुभएको डाइरेक्टरी बन्न सकेन।    ";
+$lang['ftp_unable_to_changedir']	= " डाइरेक्टरी परिवर्तन हुन सकेन।    ";
+$lang['ftp_unable_to_chmod']		= "फाइल आनुमती राख्न सकेन। पाथ जाच गर्नुहोला। ";
+$lang['ftp_unable_to_upload']		= "तोकिएको फाइल अपलोड हुन सकेन। पाथ जाच गर्नुहोला। ";
+$lang['ftp_unable_to_download']		= "तोकिएको फाइल डाउनलोड हुन सकेन। पाथ जाच गर्नुहोला। ";
+$lang['ftp_no_source_file']			= "सोर्स फाइल पत्ता लागेन। पाथ जाच गर्नुहोला। ";
+$lang['ftp_unable_to_rename']		= "फाइलको नाम परिवर्तन हुन सकेन। ";
+$lang['ftp_unable_to_delete']		= "फाइल मेटिन सकेन ।  ";
+$lang['ftp_unable_to_move']			= "फाइलको ठाउँ परिवर्तन हुन सकेन। गन्तब्य डिरेक्टोरी भएको पक्का गर्नुहोस्। ";
 
 
 /* End of file ftp_lang.php */
